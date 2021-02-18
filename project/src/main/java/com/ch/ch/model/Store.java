d568d4cd7de8e955517070b8b0a67968aaf7dab9
admin <admin@DESKTOP-R46KJ3T>
2021-02-18 17:25:55 +0900

    model

--- project/src/main/java/com/ch/ch/model/Store.java
+++ project/src/main/java/com/ch/ch/model/Store.java
@@ -1,8 +1,30 @@
 package com.ch.ch.model;
 
+
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class Store {
+	
+	private int s_num;
+	private String s_title;
+	private String s_del;
+	private String s_Pclass;
+	private String s_Pname;
+	private String s_Pconfig;
+	private String s_Pimage;
+	
+	private Date s_per;
+	private Date s_validity;
+	
+	private int s_total;
+	private int s_purchase;
+	private int s_prive;
+	private int s_sale;
+	private String del;
+	
+	private String t_account;
 
 }
